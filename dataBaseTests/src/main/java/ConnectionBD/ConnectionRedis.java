0ef1c2974a6202a7d3c122a39a5fc60058be6748
g.gonfiantini <gaetan.gonfiantini@gmail.com>
2023-04-24 16:52:16 +0200

    Ajout de la ConnectionRedis

--- dataBaseTests/src/main/java/ConnectionBD/ConnectionRedis.java
+++ dataBaseTests/src/main/java/ConnectionBD/ConnectionRedis.java
@@ -1,4 +1,45 @@
 package ConnectionBD;
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
 public class ConnectionRedis {
+
+    private static ConnectionRedis instance;
+    private JedisPool jedisPool;
+
+    private static final String HOST = "localhost";
+    private static final int PORT = 6379;
+
+    private ConnectionRedis() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxTotal(10);
+        jedisPoolConfig.setMinIdle(2);
+        jedisPoolConfig.setMaxIdle(5);
+        jedisPoolConfig.setTestOnBorrow(true);
+        jedisPoolConfig.setTestOnReturn(true);
+        jedisPool = new JedisPool(jedisPoolConfig, HOST, PORT);
+    }
+
+    public static ConnectionRedis getInstance() {
+        if (instance == null) {
+            synchronized (ConnectionRedis.class) {
+                if (instance == null) {
+                    instance = new ConnectionRedis();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public Jedis getConnection() {
+        return jedisPool.getResource();
+    }
+
+    public void closeConnection(Jedis jedis) {
+        if (jedis != null) {
+            jedis.close();
+        }
+    }
 }
--- dataBaseTests/src/main/java/MongoDBCRUD/READMongoDB.java
+++ dataBaseTests/src/main/java/MongoDBCRUD/READMongoDB.java
@@ -24,11 +24,8 @@ public class READMongoDB {
     private static final ConnectionMongoDB instanceDeConnection = ConnectionMongoDB.getInstance();
 
     public static void main(String[] args) {
-        //readCollection("testCollection");
-        //readCollectionWithHuman("testCollection");
-        readCollectionWithAge("testCollection");
+        getHuman("testCollection");
     }
-
 
     /**
      * Cette méthode permet de lire tous les documents dans une collection.
@@ -53,60 +50,27 @@ public class READMongoDB {
         System.err.println("Collection " + collectionName + " does not exist");
     }
 
-    /**
-     * Cette méthode me permet d'avoir les documents ou il y a la valeur "Human" dedans
-     */
-    public static void readCollectionWithHuman(String collectionName) {
-        MongoCollection<Document> collection = instanceDeConnection.getDatabase().getCollection(collectionName);
-        Bson filter = Filters.exists("metadata");
-        FindIterable<Document> documents = collection.find(filter);
-        for (Document document : documents) {
-            if (document.get("metadata").toString().contains("Human")) {
-                System.out.println(document);
-            }
-        }
-    }
 
     /**
-     * Cette méthode d'avoir les documents ou il y a la valeur Age > 30
+     * Méthode qui renvoie les documents dans lesquelles il y a un humain et qu'il a l'attribut NoBag = 1.00
      */
-    public static void readCollectionWithAge(String collectionName) {
-        MongoCollection<Document> collection = instanceDeConnection.getDatabase().getCollection(collectionName);
-        JsonWriterSettings settings = JsonWriterSettings.builder().outputMode(JsonMode.RELAXED).build();
+    public static void getHuman(String collectionName) {
+        MongoIterable<String> collectionsNames = instanceDeConnection.getDatabase().listCollectionNames();
+        //Je vérifie si la collection existe
+        for (String name : collectionsNames) {
 
-        for (Document document : collection.find()) {
-            String jsonString = document.get("metadata", Document.class).toJson(settings);
-            JSONObject jsonObject = new JSONObject(jsonString);
-            List<Object> values = searchInJson(jsonObject, "content");
-            for (Object value : values) {
-                if(value.toString().equals("Human")){
-                    System.out.println(document);
+            if (name.equals(collectionName)) {
+                MongoCollection<Document> collection = instanceDeConnection.getDatabase().getCollection(collectionName);
+                //J'affiche tous les documents de la collection
+                for (Document document : collection.find()) {
+
+                    if (document.get("NoBag").equals(1.00)) {
+                        System.out.println(document);
+                    }
                 }
+                return;
             }
-        }
-    }
 
-    public static List<Object> searchInJson(JSONObject jsonObject, String targetKey) {
-        List<Object> values = new ArrayList<>();
-        searchInJsonRecursively(jsonObject, targetKey, values);
-        return values;
-    }
-
-    private static void searchInJsonRecursively(Object object, String targetKey, List<Object> values) {
-        if (object instanceof JSONObject) {
-            JSONObject jsonObject = (JSONObject) object;
-            for (String key : jsonObject.keySet()) {
-                if (key.equals(targetKey)) {
-                    values.add(jsonObject.get(key));
-                } else {
-                    searchInJsonRecursively(jsonObject.get(key), targetKey, values);
-                }
-            }
-        } else if (object instanceof JSONArray) {
-            JSONArray jsonArray = (JSONArray) object;
-            for (int i = 0; i < jsonArray.length(); i++) {
-                searchInJsonRecursively(jsonArray.get(i), targetKey, values);
-            }
         }
     }
 

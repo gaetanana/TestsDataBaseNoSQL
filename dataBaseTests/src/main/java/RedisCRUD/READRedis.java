f48259a9f462550166ec8195e872be9fd12184e4
g.gonfiantini <gaetan.gonfiantini@gmail.com>
2023-04-25 12:15:31 +0200

    Ajout d'une fonction readOneKeyValue qui affiche le contenue d'une clé passé en paramètre

--- dataBaseTests/src/main/java/RedisCRUD/READRedis.java
+++ dataBaseTests/src/main/java/RedisCRUD/READRedis.java
@@ -1,4 +1,26 @@
 package RedisCRUD;
-
+import ConnectionBD.ConnectionRedis;
 public class READRedis {
+
+    private static final ConnectionRedis instanceDeConnection = ConnectionRedis.getInstance();
+
+    public static void main(String[] args) {
+        readOneKeyValue("Onvif_Metadata_C1000_2023-04-21_16-44-55.650.xml");
+    }
+
+    /**
+     * Cette fonction permet de retrouver la valeur d'une clé dans Redis
+     */
+    public static void readOneKeyValue(String nameKey) {
+        try {
+            // Récupère la valeur de la clé spécifiée
+            String storedContent = instanceDeConnection.getConnection().get(nameKey);
+            System.out.println("Contenu JSON stocké dans Redis : \n" + storedContent);
+        } finally {
+            // Ferme la connexion à Redis
+            if (instanceDeConnection.getConnection() != null) {
+                instanceDeConnection.getConnection().close();
+            }
+        }
+    }
 }
--- dataBaseTests/src/main/java/RedisCRUD/TestConnectionRedis.java
+++
@@ -1,9 +0,0 @@
-package RedisCRUD;
-import ConnectionBD.ConnectionRedis;
-public class TestConnectionRedis {
-
-    public static void main(String[] args) {
-        ConnectionRedis connectionRedis = ConnectionRedis.getInstance();
-        connectionRedis.getConnection();
-    }
-}
